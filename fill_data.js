d031fc8d6657b1233a0eaaa41ff42941761e7bc3
drstdugar <dugar.d1998@gmail.com>
2021-12-10 17:50:20 +0545

    Style practice changes

---
+++ fill_data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    title: "GitHub Assignment 2",
+    code: "https://github.com/drstdugar/LeapFrog_Assignment/blob/master/index.html",
+    demo: "https://drstdugar.github.io/LeapFrog_Assignment/",
+  },
+];
+
+loadData(data);
+
+function loadData(data) {
+  const table = document.getElementById("table_data");
+  data.forEach((item) => {
+    let row = table.insertRow();
+    let title = row.insertCell(0);
+    title.innerHTML = item.title;
+    let code = row.insertCell(1);
+    code.innerHTML = `<a href=${item.code}>Click here for Code</a>`;
+    let demo = row.insertCell(2);
+    demo.innerHTML = `<a href="${item.demo}">Click here for Demo</a>`;
+  });
+}

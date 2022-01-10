a2079c13e683ae8800a27338890a82694a062a68
drstdugar <dugar.d1998@gmail.com>
2022-01-10 18:53:50 +0545

    Fix Ballons disappear

--- JavaScript/Final_Project/script/balloons/words.js
+++ JavaScript/Final_Project/script/balloons/words.js
@@ -27,7 +27,7 @@ export const wordList = [
 
 export function generateLetters() {
   const letters = [];
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 7; i++) {
     let word = wordList[Math.floor(Math.random() * wordList.length)];
     letters.push(...word.split(''));
   }
